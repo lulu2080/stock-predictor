fe2733d1d696606ad856bb0b7fe8d68144b9d095
lulu2080 <lu_zhaohui@aliyun.com>
2017-11-16 18:11:35 +0800

    新增批量预测所有期货合约品种走势程序及正确率评估程序。

--- eval_future.py
+++ eval_future.py
@@ -1,91 +1,57 @@
 #encoding:utf-8
 #! /usr/bin/env python
 
-import os
-import sys
-import tensorflow as tf
-import numpy as np
-import argparse
-from tensorflow.python.lib.io import file_io
-import pandas as pd
-import future_data_loader as fdl
+import pymysql as db
 
-FLAGS = None
-batch_size = 30
-time_step = 5
+sql_0 = 'select id,contract_id,predict_period,predict_time from future_predictor where actual_result=-1 ORDER BY contract_id ASC,predict_period ASC,predict_time ASC'
+sql_1 = 'SELECT trade_date, close_price from future_quote WHERE contract_id=%(contract_id)s and period_type=%(period_type)s and trade_date>=DATE_FORMAT(%(predict_time)s,\'%%Y-%%m-%%d %%H:%%i:%%s\') and open_price > 0 and close_price > 0 ORDER BY trade_date ASC'
+sql_2 = 'UPDATE future_predictor SET actual_result=%(actual_result)s where id=%(id)s'
 
-# Parameters
-# ==================================================
-parser = argparse.ArgumentParser()
-#获得期货合约代码
-parser.add_argument('--contractCode', type=str, default='CU0',
-        help='future contract code')
-#获得checkpoint路径
-parser.add_argument('--checkpointDir', type=str, default='model/cloud/runs/1510136356/checkpoints/',
-                        help='output model path')
-FLAGS, _ = parser.parse_known_args()
+period_dict = {30 : 5}
+rc_dict = {30 : 6}
 
-eval_file_path, lastTradingDay = fdl.get_future_data(FLAGS.contractCode, 5)
-
-if eval_file_path is None:
-    print(-1)
-    sys.exit(0)
-
-#ali-oss read mode
-with file_io.FileIO(eval_file_path, mode="r") as f:
-    df=pd.read_csv(f)     #读入期货数据
-    data_x=df.iloc[:,0:3].values   #取前3列
-os.remove(eval_file_path)
-
-mean = np.mean(data_x, axis=0)
-std = np.std(data_x, axis=0)
-normalized_eval_data = (data_x-mean) / std  #标准化
-len_data = len(normalized_eval_data)
-
-batch_index = []
-eval_x = []
-#for i in range(size - 1):
-for i in range(len_data - time_step + 1):
-    #构建一个batch
-    if i % batch_size == 0:
-        batch_index.append(i)
-    #构建一个序列(time_step)
-#   x = normalized_eval_data[len_data-(i+1)*time_step:len_data-i*time_step]
-    x = normalized_eval_data[len_data-time_step-i:len_data-i]
-    eval_x.append(x.tolist())
-#eval_x = eval_x[::-1]
-batch_index = batch_index[::-1]
-#print(eval_x)
-
-# Evaluation
-# ==================================================
-
-graph = tf.Graph()
-with graph.as_default():    
-    checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpointDir)
-    with tf.Session() as sess:
-#        # Initialize all variables
-        sess.run(tf.local_variables_initializer())
-        sess.run(tf.global_variables_initializer())
+def main():
+    try:
+        conn = db.Connect(host='101.37.60.132', 
+                          user='root', passwd='funci@868', db='collected_data', port=3306, charset='utf8')
+        cur = conn.cursor()
         
-        # Load the saved meta graph and restore variables
-        saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
-        saver.restore(sess, checkpoint_file)
+        #获取未验证的预测记录
+        rowcount = cur.execute(sql_0)
+        print("共查询到{:d}条未验证的期货走势预测记录。\n".format(rowcount))
 
-        # Get the placeholders from the graph by name
-        input_x = graph.get_operation_by_name("input_x").outputs[0]
-        p_result = graph.get_operation_by_name("p_result").outputs[0]
-        p_rate = graph.get_operation_by_name("p_rate").outputs[0]
-        # Tensors we want to evaluate
-        get_result = graph.get_operation_by_name("get_result").outputs[0]
-        get_rate = graph.get_operation_by_name("get_rate").outputs[0]
+        results = cur.fetchall()
+        for row in results:
+            id = row[0]
+            contract_id = row[1]
+            predict_period = row[2]
+            predict_time = row[3]
 
-        for i in range(len(batch_index) - 1):
-            x_batch = eval_x[batch_index[i + 1]:batch_index[i]]
-            sess.run([get_result, get_rate], {input_x: x_batch[0:]})
+            print('Evaluating:' + 'contract_id='+ str(contract_id) + ',predict_period=' + str(predict_period) + ',predict_time=' + predict_time.strftime('%Y-%m-%d %H:%M:%S') + ' ...', end='')
+            
+            value = {'contract_id':contract_id, 'period_type':period_dict[predict_period], 'predict_time':predict_time}
+            rowcount = cur.execute(sql_1, value)
+            if rowcount > rc_dict[predict_period]:
+                qr = cur.fetchall()
+                if abs(qr[6][1] - qr[0][1]) / qr[0][1] <= 0.005:
+                    actual_result = 0
+                elif qr[6][1] < qr[0][1]:
+                    actual_result = 1
+                else:
+                    actual_result = 2
+                
+                value = {'id':id, 'actual_result':actual_result}
+                cur.execute(sql_2, value)
+                conn.commit()
+                
+                print('actual_result=' + str(actual_result) + '   ok!')
+            else:
+                print('result has not come out yet.')
+    except db.Error as e:
+        print("Mysql Error {:d}:{:s}\n".format(e.args[0], e.args[1]))
+    finally:
+        cur.close()
+        conn.close()    
 
-        pred_result, pred_rate = sess.run([p_result, p_rate])
-        
-        date_str = lastTradingDay.strftime('%Y-%m-%d %H:%M:%S')
-
-        print(str(pred_result) + "," + str(pred_rate[pred_result]) + "," + date_str)
+if __name__ == '__main__':
+    main()
---
+++ future_batch_predictor.py
@@ -0,0 +1,50 @@
+#encoding:utf-8
+#! /usr/bin/env python
+
+import os
+import pymysql as db
+
+sql_0 = 'select id, contract_code from future_contract where status=0 ORDER BY id ASC'
+sql_1 = 'select 1 from future_predictor where contract_id=%(contract_id)s and predict_period=%(predict_period)s and predict_time=DATE_FORMAT(%(predict_time)s,\'%%Y-%%m-%%d %%H:%%i:%%s\')'
+sql_2 = 'INSERT INTO future_predictor(contract_id, predict_period, predict_time, predict_result, acc_rate, actual_result) VALUES(%(contract_id)s, %(predict_period)s, %(predict_time)s, %(predict_result)s, %(acc_rate)s, %(actual_result)s)'
+sql_3 = 'UPDATE future_predictor SET predict_result=%(predict_result)s, acc_rate=%(acc_rate)s where contract_id=%(contract_id)s and predict_period=%(predict_period)s and predict_time=DATE_FORMAT(%(predict_time)s,\'%%Y-%%m-%%d %%H:%%i:%%s\')'
+sql_4 = 'select 1 from future_predictor where contract_id=%(contract_id)s and predict_period=%(predict_period)s and predict_time=DATE_FORMAT(%(predict_time)s,\'%%Y-%%m-%%d %%H:%%i:%%s\')'
+
+def main():
+    try:
+        conn = db.Connect(host='101.37.60.132', 
+                          user='root', passwd='funci@868', db='collected_data', port=3306, charset='utf8')
+        cur = conn.cursor()
+        
+        #获取所有股票内码列表
+        rowcount = cur.execute(sql_0)
+        print("共查询到{:d}个期货合约。\n".format(rowcount))
+
+        results = cur.fetchall()
+        for row in results:
+            contract_id = row[0]
+            contract_code = row[1]
+            ret = os.popen('python future_service.py --contractCode ' + str(contract_code))
+            data = ret.readlines()[-1].strip().split(',')
+            if not data[0].isdigit():
+                continue
+            if int(data[0]) == -1:
+                continue
+
+            value = {'contract_id':contract_id, 'predict_period':30, 'predict_time':data[2], 'predict_result':data[0], 'acc_rate':data[1], 'actual_result':0}
+
+            rowcount = cur.execute(sql_1, value)
+            if rowcount <= 0:
+                cur.execute(sql_2, value)
+            else:
+                cur.execute(sql_3, value)
+
+            conn.commit()
+    except db.Error as e:
+        print("Mysql Error {:d}:{:s}\n".format(e.args[0], e.args[1]))
+    finally:
+        cur.close()
+        conn.close()
+
+if __name__ == '__main__':
+    main()
